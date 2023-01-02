178340935b97964252d352fda48817abf9c52f68
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2023-01-02 12:19:59 +0200

    task done

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -21,8 +21,14 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
+//            int number = Integer.parseInt(next);
             // todo: complete it
+            try {
+                sum += Integer.parseInt(next);
+
+            } catch (NumberFormatException e) {
+                justWords += " " + next;
+            }
         }
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
